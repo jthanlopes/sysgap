37c41dd8f8953a4acd7e0b55521c5dc7a5925da7
jthanlopes <jthan.lopes@gmail.com>
2017-10-12 22:46:37 -0300

    Cadastro e exibição das imagens das postagens.

--- app/Http/Controllers/Web/Empresa/Noticia/NoticiaController.php
+++ app/Http/Controllers/Web/Empresa/Noticia/NoticiaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web\Empresa\Noticia;
 use App\Noticia;
 use Auth;
 use Illuminate\Http\Request;
+use Storage;
 
 class NoticiaController extends Controller
 {  
@@ -12,13 +13,16 @@ class NoticiaController extends Controller
     $this->middleware('auth:empresa');
   }  
 
-  public function criarNoticia(Request $request) {        
-    $file = 'teste';
+  public function criarNoticia(Request $request) {
+    // $filename = config('app.name') . $request->file('imagem')->getClientOriginalName();
+    $filename = config('app.name') . '_post_' . Auth::user()->id . '_' . $request->file('imagem')->getClientOriginalName();
+    $storage = 'empresas/posts/' .  str_slug(Auth::user()->nome, '_');
+    $request->imagem->storeAs($storage, $filename, 'public');
     
     $create = Noticia::create([
       'titulo' => request('titulo'),
       'conteudo' => request('conteudo'),
-      'imagem' => $file,
+      'imagem' => $filename,
       'empresa_id' => Auth::user()->id,
       'ativo' => 1,
       'principal' => 0,
--- resources/views/site/Empresa/perfil.blade.php
+++ resources/views/site/Empresa/perfil.blade.php
@@ -12,13 +12,13 @@
             <h6 class="w3-opacity" style="cursor: pointer;">Publique aqui notícias e/ou eventos.</h6>      
           </div>
           <div class="form-publicar">
-            <form method="POST" action="{{ route('noticia.novo') }}">
+            <form method="POST" action="{{ route('noticia.novo') }}" enctype="multipart/form-data">
               {{ csrf_field() }}
               <p><input type="text" name="titulo" placeholder="Título da publicação"></p>
               <p><textarea name="conteudo" id="" cols="30" rows="10" placeholder="Conteúdo da publicação"></textarea></p>
               <label for="">Imagem do post:</label>
-              <p><input type="file"></p>
-              <button type="input" name="imagem_noticia" class="w3-button w3-theme"><i class="fa fa-pencil"></i> &nbsp;Postar</button>
+              <p><input type="file" name="imagem"></p>
+              <button type="input" name="postar" class="w3-button w3-theme"><i class="fa fa-pencil"></i> &nbsp;Postar</button>
             </form>
           </div>
         </div>
@@ -29,12 +29,12 @@
   @foreach($noticias as $noticia)
   <div class="w3-container w3-card-2 w3-white w3-round w3-margin"><br>
     <span class="w3-right w3-opacity">{{ $noticia->created_at->diffForHumans() }} {{-- 1 min --}}</span>
-    <h4>{{ $noticia->titulo }}</h4><br>
+    <h4>{{ $noticia->titulo }} <span class="opt-post">[<a href="">Editar</a>, <a href="">Excluir</a>]</span></h4><br>
     <hr class="w3-clear">
     <p>{{ $noticia->conteudo }}</p>
     <div class="w3-row-padding" style="margin:0 -16px">
-      <div class="w3-half">
-        <img src="/w3images/lights.jpg" style="width:100%" alt="Northern Lights" class="w3-margin-bottom">
+      <div class="w3-half">        
+        <img src="{{ asset('storage')  . '/empresas/posts/' . Auth::user()->nome . '/' . $noticia->imagem  }}" style="width:100%" alt="Imagem do Post" class="w3-margin-bottom">    
       </div>      
     </div>    
   </div>
