d685eca40d3ca99d7caa26fad47951d9f649b68a
Jonathan Lopes <Jonathan Lopes>
2017-10-29 02:40:47 -0200

    Ajuste reCAPTCHA.

--- config/services.php
+++ config/services.php
@@ -15,28 +15,34 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+      'domain' => env('MAILGUN_DOMAIN'),
+      'secret' => env('MAILGUN_SECRET'),
     ],
 
     'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => 'us-east-1',
+      'key' => env('SES_KEY'),
+      'secret' => env('SES_SECRET'),
+      'region' => 'us-east-1',
     ],
 
     'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
+      'secret' => env('SPARKPOST_SECRET'),
     ],
 
     'stripe' => [
-        'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+      'model' => App\User::class,
+      'key' => env('STRIPE_KEY'),
+      'secret' => env('STRIPE_SECRET'),
     ],
 
     'recaptcha' => [
-        'secret' => env('RECAPTCHA_SECRET'),
-    ]
+      'secret' => env('RECAPTCHA_SECRET'),
+    ],
 
-];
+    'recaptcha' => [
+      'public' => env('RECAPTCHA_PUBLIC'),
+    ],
+
+
+
+  ];
